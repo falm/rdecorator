e4e9cb69b1d19238046668106a91914e89e900a2
Jason Hou <hjj1992@gmail.com>
2016-12-13 16:40:00 +0800

    dummy class

--- spec/rdecorator_spec.rb
+++ spec/rdecorator_spec.rb
@@ -2,6 +2,52 @@ require 'spec_helper'
 
 describe Rdecorator do
 
+  class Decorator
+
+    def initialize(this, *args)
+
+    end
+
+    def call
+
+    end
+
+  end
+
+  module DecoratorMethods
+
+    def prefix(*)
+      'prefix' << yield
+    end
+
+    def suffix(*)
+       yield << 'suffix'
+    end
+
+  end
+
+  class DummyClass
+
+    extend Rdecorator
+    include DecoratorMethods
+
+    wrap :prefix
+    def first
+      'first'
+    end
+
+    around :suffix
+    def second
+      'second'
+    end
+
+    wrap Decorator
+    def third
+      'third'
+    end
+
+  end
+
   it '' do
 
   end
